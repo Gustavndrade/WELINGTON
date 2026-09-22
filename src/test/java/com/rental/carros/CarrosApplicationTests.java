c0a7dd650dee81deb0bf9b30ad56ab1126f0eb52
Gustavo barbosa <gustavo.barbosa@go4digital.com.br>
2026-09-22 11:17:06 -0300

    >>>

---
+++ src/test/java/com/rental/carros/CarrosApplicationTests.java
@@ -0,0 +1,12 @@
+package com.rental.carros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
